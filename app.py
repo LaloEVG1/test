5216ccf98bde1029623b063e4d967513bb32cd70
LaloEVG1 <eduardo_vala@hotmail.com>
2025-02-26 10:33:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -153,13 +153,16 @@ if st.session_state.authenticated:
                 st.session_state.culvert_estimates = []
                 st.warning("All estimates have been cleared!")
                 st.rerun()
+
+            
                 
-    
+            updated_df = pd.DataFrame(st.session_state.culvert_estimates)
+            
                 # === PIE CHART: COST PER TYPE ===
             st.subheader("Cost Distribution by Culvert Type")
     
             # Group by "type" and sum the subtotals
-            type_totals = df.groupby("Tipo")["Subtotal"].sum()
+            type_totals = updated_df.groupby("Tipo")["Subtotal"].sum()
     
             if not type_totals.empty:
                 # Create Pie Chart
